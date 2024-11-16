8c4803e761a5a89944f400ea05f89687e8112e52
Luiz Hélio <luizhelionerd@hotmail.com>
2024-11-16 15:12:20 -0300

    Create Administrador.java
    Classe Administrador

---
+++ Pessoas/Administrador.java
@@ -0,0 +1,29 @@
+package Pessoas;
+
+import Filmes.Locadora;
+
+public class Administrador extends Pessoa {
+	public Administrador(int id, String nome, String email, String senha, String endereco) {
+		super(id, nome, email, senha, endereco);
+	}
+
+	public boolean adicionarUsuario(Cliente user) {
+		
+	}
+	
+	public boolean removerUsuario(Cliente user) {
+		
+	}
+	
+	public boolean listarUsuarios() {
+		
+	}
+	
+	public String verificarHistoricoCliente (Cliente cliente) {
+		
+	}
+	
+	public void gerenciarReservas() {
+		
+	}
+}
