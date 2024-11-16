28440b216c304ecda00d345ee0f431bb4f3a2402
Luiz Hélio <luizhelionerd@hotmail.com>
2024-11-16 15:12:26 -0300

    Create Cliente.java
    Classe Cliente

---
+++ Pessoas/Cliente.java
@@ -0,0 +1,21 @@
+package Pessoas;
+
+import java.util.ArrayList;
+import Filmes.Filme;
+
+public class Cliente extends Pessoa {
+	public Cliente(int id, String nome, String email, String senha, String endereco) {
+		super(id, nome, email, senha, endereco);
+	}
+
+
+	public boolean iniciarLocacao (Filme filme) {
+		
+	}
+	
+	public String visualizarHistorico() {
+		
+	}
+	
+	
+}
