0b018a6930ac51fcf793ec66bd2024782f19fb51
Luiz Hélio <luizhelionerd@hotmail.com>
2024-11-20 18:13:29 -0300

    Update Administrador.java

--- Pessoas/Administrador.java
+++ Pessoas/Administrador.java
@@ -1,29 +1,76 @@
 package Pessoas;
 
-import Filmes.Locadora;
+import java.util.ArrayList;
+import Filmes.Filme;
+import Filmes.Locacao;
 
 public class Administrador extends Pessoa {
+
 	public Administrador(int id, String nome, String email, String senha, String endereco) {
 		super(id, nome, email, senha, endereco);
 	}
+	
+	public boolean removerUsuario(Pessoa user, ArrayList<Pessoa> usuarios) {
+		if (usuarios.remove(user)) {
+			System.out.println("Usuário removido com sucesso: " + user.getNome());
+			return true;
+		} else {
+			System.out.println("Usuário não encontrado.");
+			return false;
+		}
+	}
+	
+	public void listarUsuarios(ArrayList<Pessoa> usuarios) {
+		if (usuarios.isEmpty()) {
+			System.out.println("Nenhum usuário encontrado.");
+		} else {
+			System.out.println("Lista de Usuários: ");
+			for (Pessoa usuario : usuarios) {
+				System.out.println(usuario);
+			}
+		}
+	}
+	
+	public boolean adicionarFilme(Filme filme, ArrayList<Filme> catalogoFilmes) {
+		if (filme != null) {
+			catalogoFilmes.add(filme);
+			System.out.println("Filme Adicionado com Sucesso:" + filme.getNome());
+			return true;
+		} else {
+			System.out.println("Filme inválido.");
+			return false;
+		}
+	}
+	
+	public boolean removerFilme(Filme filme, ArrayList<Filme> catalogoFilmes) {
+		if (catalogoFilmes.remove(filme)) {
+			System.out.println("Filme removido com sucesso:" + filme.getNome());
+			return true;
+		} else {
+			System.out.println("Filme não encontrado.");
+			return false;
+		}
+	}
+	
+	public boolean verificarHistoricoCliente(Cliente cliente) {
+		if (cliente != null) {
+			cliente.visualizarHistorico();
+			return true;
+		} else {
+			System.out.println("Cliente inválido.");
+			return false;
+		}
+	}
+	
+	public void gerenciarReserva(ArrayList<Locacao> reservas) {
+		if (reservas.isEmpty()) {
+			System.out.println("Nenhuma reserva encontrada.");
+		} else {
+			System.out.println("Reservas realizadas: ");
+			for(Locacao reserva : reservas) {
+				System.out.println(reserva);
+			}
+		}
+	}
 
-	public boolean adicionarUsuario(Cliente user) {
-		
-	}
-	
-	public boolean removerUsuario(Cliente user) {
-		
-	}
-	
-	public boolean listarUsuarios() {
-		
-	}
-	
-	public String verificarHistoricoCliente (Cliente cliente) {
-		
-	}
-	
-	public void gerenciarReservas() {
-		
-	}
 }
