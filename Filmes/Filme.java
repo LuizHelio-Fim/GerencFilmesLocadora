a6beb968628df8a9986a4beb2b15a00ae4e90af8
Luiz Hélio <luizhelionerd@hotmail.com>
2024-11-20 18:12:42 -0300

    Update Filme.java

--- Filmes/Filme.java
+++ Filmes/Filme.java
@@ -1,86 +1,97 @@
 package Filmes;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Filme {
-	private int id;
-	private String nome;
-	private String genero;
-	private String diretor;
-	private String dataLançamento;
-	private boolean disponivel;
-	
-	//Construtores
-	public Filme(int id, String nome, String genero, String diretor, String dataLançamento, boolean disponivel) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.genero = genero;
-		this.diretor = diretor;
-		this.dataLançamento = dataLançamento;
-		this.disponivel = disponivel;
-	}
-	
-	public Filme() {
-		super();
-		this.id = 0;
-		this.nome = "";
-		this.genero = "";
-		this.diretor = "";
-		this.dataLançamento = "00/00/0000";
-		this.disponivel = true;
-	}
+    private int id;
+    private String nome;
+    private String genero;
+    private String diretor;
+    private LocalDate dataLancamento;
+    private boolean disponivel;
 
-	//Getters e Setters
-	public int getId() {
-		return id;
-	}
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy/MM/dd");
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    // Construtores
+    public Filme(int id, String nome, String genero, String diretor, LocalDate dataLancamento, boolean disponivel) {
+        this.id = id;
+        this.nome = nome;
+        this.genero = genero;
+        this.diretor = diretor;
+        this.dataLancamento = dataLancamento;
+        this.disponivel = disponivel;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public Filme() {
+        this.id = 0;
+        this.nome = "";
+        this.genero = "";
+        this.diretor = "";
+        this.dataLancamento = LocalDate.now();
+        this.disponivel = true;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
 
-	public String getGenero() {
-		return genero;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getDiretor() {
-		return diretor;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setDiretor(String diretor) {
-		this.diretor = diretor;
-	}
+    public String getGenero() {
+        return genero;
+    }
 
-	public String getDataLançamento() {
-		return dataLançamento;
-	}
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 
-	public void setDataLançamento(String dataLançamento) {
-		this.dataLançamento = dataLançamento;
-	}
+    public String getDiretor() {
+        return diretor;
+    }
 
-	public boolean isDisponivel() {
-		return disponivel;
-	}
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
 
-	public void setDisponivel(boolean disponivel) {
-		this.disponivel = disponivel;
-	}
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
 
-	@Override
-	public String toString() {
-		return "ID: " + id + "\nNome: " + nome + "\nGenero: " + genero + "\nDiretor: " + diretor
-				+ "\n Lançamento: " + dataLançamento + "\n Disponivel: " + disponivel + "\n\n";
-	}
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public String getDataLancamentoFormatada() {
+        return this.dataLancamento.format(FORMATTER);
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = LocalDate.parse(dataLancamento, FORMATTER);
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + "\nNome: " + nome + "\nGenero: " + genero + "\nDiretor: " + diretor
+                + "\nLançamento: " + dataLancamento.format(FORMATTER) + "\nDisponivel: " + disponivel + "\n\n";
+    }
 }
