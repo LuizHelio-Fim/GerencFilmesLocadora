1c04f4e1ff64b1fb6b8f19228c4150a8f7f9d039
Luiz Hélio <luizhelionerd@hotmail.com>
2024-11-23 13:28:29 -0300

    Create Pagamento.java

---
+++ Filmes/Pagamento.java
@@ -0,0 +1,49 @@
+package Filmes;
+
+import java.time.LocalDate;
+import Pessoas.Cliente;
+
+public class Pagamento {
+	
+	Locacao locacao;
+	double valor;
+	Cliente cliente; 
+	LocalDate dataPagamento;
+	boolean status;
+	
+	public Pagamento(Locacao locacao, LocalDate dataPagamento) {
+
+		this.locacao = locacao;
+		this.dataPagamento = dataPagamento;
+	}
+
+	public double calcularMultaPorAtraso ( ){
+		
+		int multa = (dataPagamento.getDayOfYear() -  this.locacao.getDataDevolucaoPrevista().getDayOfYear()) * 5;
+		return multa;
+	}
+	
+	public double calculaValorFinal () {
+		
+		int periodoLocado = locacao.getDataDevolucaoPrevista().getDayOfYear() - locacao.getDataLocacao().getDayOfYear();
+		
+		double preco = periodoLocado * this.locacao.getFilme().getPreco();
+		
+		double multa = calcularMultaPorAtraso();
+		
+		double precoFinal = preco + multa;
+		
+		return precoFinal;
+		
+	}
+	
+	public boolean processarPagamento(double pagamento) {
+		System.out.println("Processando...");
+		if (pagamento == calculaValorFinal()) {
+			System.out.println("Pagamento confirmado!");
+			return true;
+		}
+		System.out.println("[ERRO] - Pagamento negado!");
+		return false;
+	}
+}
